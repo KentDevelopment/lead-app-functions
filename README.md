09b7e6919abc3ee0f7a9a3a775a683ef0176073e
Renan Sigolo <renan.sigolo@gmail.com>
2018-06-26 01:00:31 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Kent LEAD Points &middot; Functions
+
+The LEAD Points app has been develop to foment the student engagement with Kent's events, workshops and social activities.
+
+## Deploying to production
+
+To deploy firebase functions to productions run `yarn deploy`
+
+## Built With
+
+* [Firebase](https://firebase.google.com/) - Used as host, storage and fireStore as a DB.
+* [Nodemailer](https://nodemailer.com/about/) - Nodemailer is a module for Node.js to send emails.
+
+## Dev tools
+
+* [Eslint](https://eslint.org/) - The pluggable linting utility for JavaScript and JSX.
+* [Yarn](https://yarnpkg.com/en/) - Fast, reliable, and secure dependency management.
+
+## Authors
+
+* [Renan Sigolo](https://github.com/renansigolo)
+
+## License
+
+This project is licensed under the APACHE 2.0 License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* Thanks to everyone at Kent Institute
+* Inspiring the world to be more collaborative
+
+## Reference
+
+* [Firebase Function Example](https://github.com/firebase/functions-samples/blob/master/email-confirmation/README.md)
+* [Firebase CLI Documentation](https://firebase.google.com/docs/cli/)
---
+++ functions/index.js
@@ -0,0 +1,57 @@
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+const functions = require('firebase-functions')
+const nodemailer = require('nodemailer')
+
+const admin = require('firebase-admin')
+admin.initializeApp()
+
+const gmailEmail = functions.config().gmail.email
+const gmailPassword = functions.config().gmail.password
+const mailTransport = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+		user: gmailEmail,
+		pass: gmailPassword
+	}
+})
+
+exports.firestoreEmail = functions.firestore
+	.document('users/{uid}')
+	.onCreate(event => {
+		const userId = event._fieldsProto.uid.stringValue
+		const db = admin.firestore()
+		return db
+			.collection('users')
+			.doc(userId)
+			.get()
+			.then(doc => {
+				const user = doc.data()
+				console.log('USER', user)
+				const mailOptions = {
+					subject: `New user added to ${user.campus} LEAD App ${user.email}`,
+					from: '"Lead App" <development@student.kent.edu.au>',
+					to: 'lead@kent.edu.au',
+					bcc: 'renan.sigolo@gmail.com'
+				}
+
+				// Building Email message.
+				mailOptions.html = `
+					<h1>You have a new registered user on the LEADERBOARD App, please update their LEAD points as soon as possible!</h1>
+					<p><b>Email Address: </b>${user.email}</p>
+					<p><b>Name: </b>${user.displayName}</p>
+					<p><b>Campus: </b>${user.campus}</p>
+					`
+				mailOptions.text = `Email Address: ${user.email}
+				Name: ${user.displayName}
+				Campus: ${user.campus}`
+
+				return mailTransport
+					.sendMail(mailOptions)
+					.then(() => console.log(`Email has been sent`))
+					.catch(error =>
+						console.error('There was an error while sending the email:', error)
+					)
+			})
+	})
