561c0bfa484ee6242d1a8e2102c95fd97693269c
Renan Sigolo <renan.sigolo@gmail.com>
2021-06-18 12:40:16 +1000

    build: update lint and dependencies

---
+++ functions/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2017,
+  },
+  extends: ["eslint:recommended", "google", "prettier"],
+};
